f4ed25d8b14f1642ba2c60d50b2c2964ff9f71d5
Dominik Schweigl <domnik.schweigl@gmail.com>
2025-07-15 19:31:48 +0200

    feat: add classification frontend function

--- frontend/src/lib/classification.ts
+++ frontend/src/lib/classification.ts
@@ -1,29 +1,16 @@
 import type { ASLLabel, Prediction } from "@/types/prediction";
 
-const BACKEND_URL: string = "http://localhost:8000/predict";
+const BACKEND_URL: string = "http://localhost:5000/predict";
 
 export async function classify(file: File): Promise<Prediction[]> {
   const formData = new FormData();
   formData.append("file", file);
 
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-          .split("")
-          .map((c) => ({ label: c as ASLLabel, confidence: Math.random() }))
-      );
-    }, 500);
+  const res = await fetch(BACKEND_URL, {
+    method: "POST",
+    body: formData,
   });
+  const data: Prediction[] = await res.json();
 
-  //   const formData = new FormData();
-  //   formData.append("file", file);
-
-  //   const res = await fetch(BACKEND_URL, {
-  //     method: "POST",
-  //     body: formData,
-  //   });
-  //   const data: Classification = await res.json();
-
-  //   return data;
+  return data;
 }
