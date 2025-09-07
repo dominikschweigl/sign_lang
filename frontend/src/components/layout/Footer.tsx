8a555b7761838b6d79069b4d04ea3fc0d4a6d492
Dominik Schweigl <domnik.schweigl@gmail.com>
2025-09-07 07:45:54 +0200

    feat: make footer responsive

--- frontend/src/components/layout/Footer.tsx
+++ frontend/src/components/layout/Footer.tsx
@@ -14,7 +14,7 @@ interface FooterProps {
 const Footer = ({ copyright, bottomLinks }: FooterProps) => {
   return (
         <footer className="w-full md:gap-2 p-4 pb-3 md:p-6 md:py-8">
-          <div className="w-full text-black flex flex-col justify-between gap-4 border-t pt-8 text-sm font-medium md:flex-row md:items-center">
+          <div className="w-full text-black flex flex-row justify-between gap-4 border-t pt-4 md:pt-8 text-sm font-medium items-center">
             <p>{copyright}</p>
             <ul className="flex gap-4">
               {bottomLinks?.map((link, linkIdx) => (
@@ -24,13 +24,13 @@ const Footer = ({ copyright, bottomLinks }: FooterProps) => {
               ))}
             </ul>
             <div className="ml-auto flex hover:cursor-pointer">
-                <Link href="https://github.com/dominikschweigl/sign_lang" target="_blank" className="hidden lg:block">
+                <Link href="https://github.com/dominikschweigl/sign_lang" target="_blank" className="hidden sm:block">
                 <Button variant={"link"} className="gap-1.5">
                     <Github className="size-4" />
                     dominikschweigl/sign-lang
                 </Button>
                 </Link>
-                <Link href="https://github.com/dominikschweigl/sign_lang" target="_blank" className="lg:hidden">
+                <Link href="https://github.com/dominikschweigl/sign_lang" target="_blank" className="sm:hidden">
                 <Button variant={"ghost"} size={"icon"}>
                     <Github className="size-4" />
                 </Button>
